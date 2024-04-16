d35b44880a2391bac41c53ed24321e6c90a6973e
Chloë Rivière <chloerivs@gmail.com>
2024-04-16 22:23:44 +0000

    Create a function that handles player choices and updates the progress tracker based on each choice.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,48 @@ let tracker = {
     progress: 'Welcome', //first step of flowchart
 }
 
-//Function to handle player choices
+//Function to handle player choices and update progress tracker
+function playerChoice(option) {
+    switch(option){
+        //First choice
+        case 'continue':
+            tracker.progress = 'continue';
+            break;
+        case 'retreat':
+            tracker.progress = 'retreat';
+            break;
+        //Second choice
+        case 'left':
+            tracker.progress = 'left';
+            break;
+        case 'right':
+            tracker.progress = 'right';
+            break;
+        //Third choice
+        case 'potion':
+            tracker.progress = 'potion';
+            break;
+        case 'sword':
+            tracker.progress = 'sword';
+            break;
+        //Fourth choice
+        case 'keepPet':
+            tracker.progress = 'keepPet';
+            break;
+        case 'goOn':
+            tracker.progress = 'goOn';
+            break;
+        //Fifth choice
+        case 'cucumber':
+            tracker.progress = 'cucumber';
+            break;
+        case 'meow':
+            tracker.progress = 'meow';
+            break;
+    }
+    updateGame();
+}
+
 
 //Function to update game text
 
