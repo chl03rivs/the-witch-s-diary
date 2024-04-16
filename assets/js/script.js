250e787b71badb1541dff50e6ad5e6f3ab91f866
Chloë Rivière <chloerivs@gmail.com>
2024-04-16 22:14:08 +0000

    Add comments to JS file to plan use of functions. Add tracker object with default values for player lives and progres to initialise at game start

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//Set a tracker for player's remaining lives(attempts) and where they are in the game
+let tracker = {
+    lives: 3, //default/max lives
+    progress: 'Welcome', //first step of flowchart
+}
+
+//Function to handle player choices
+
+//Function to update game text
+
+//Function to launch the game when the player presses the start button
